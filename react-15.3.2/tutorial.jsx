9ddf4bfd2877b1ff28f10fd8a05b3acb2c47a21d
Damanvir Mangat <daman@Damanvirs-MacBook-Pro.local>
2016-10-17 12:19:40 -0400

    looking good

--- react-15.3.2/tutorial.jsx
+++ react-15.3.2/tutorial.jsx
@@ -3,6 +3,7 @@ var model = [
     {
         name: "Home",
         description: "My Home. I have lived here for most of my life, moving in during March break of 1997, I have so many cherished memories of this place!",
+        key: 1,
         coordinates: {
             lat: 43.22047260000001,
             lng: -79.6472996
@@ -10,6 +11,7 @@ var model = [
     }, {
         name: "Memphis Barbeque",
         description: "One of the best and most popular restaurants in all of Stoney Creek. A classic smokehouse offering some of the finest Barbeque food you could ever ask for",
+        key: 2,
         coordinates: {
             lat: 43.209,
             lng: -79.666
@@ -17,204 +19,106 @@ var model = [
     }
 ];
 
-var octopus = {
-    getMarkers: function() {
-        model.map(function(marker) {
-            console.log(marker)
-        });
-    },
-
-    getMarkerNames: function() {
-        var names = [];
-        model.map(function(marker) {
-            names.push(marker.name);
-        });
-        return names;
-    },
-
-    markerCoordinates: function() {
-        var coords = [];
-        for (let i = 0; i < model.length; i++) {
-            coords[i] = [model[i].coordinates.lat, model[i].coordinates.lng];
-        }
-        return coords;
-
-    }
-};
-
-
-        var middleMapSettings = {
-            center: {
-                lat: 43.227,
-                lng: -79.719
-            },
-            zoom: 11,
-            styles: [
-                {
-                    featureType: 'all',
-                    stylers: [
-                        {
-                            saturation: -10
-                        }, {
-                            invert_lightness: true
-                        }, {
-                            gamma: 1.2
-                        }
-
-                    ]
-                }, {
-                    featureType: 'landscape.natural',
-                    stylers: [
-                        {
-                            saturation: -80
-                        }, {
-                            lightness: -50
-                        }
-                    ]
-                }, {
-                    featureType: 'water',
-                    stylers: [
-                        {
-                            saturation: -10
-                        }, {
-                            lightness: -60
-                        }
-                    ]
-                }, {
-                    featureType: 'road.highway',
-                    elementType: 'geometry',
-                    stylers: [
-                        {
-                            color: "#4b4b4b"
-                        }
-
-                    ]
-                }, {
-                    featureType: 'poi.park',
-                    stylers: [
-                        {
-                            saturation: -90
-                        }
-                    ]
-                }
-            ]
-        };
-
-
-//         var map = new google.maps.Map(document.getElementById('map'), middleMapSettings);
-// // dynamically set the markers and the info window for them
-//         function populate() {
-//             var daz = [];
-//             for (let i = 0; i < model.length; i++) {
-//                 daz[i] = new google.maps.Marker({
-//                     position: {
-//                         lat: model[i].coordinates.lat,
-//                         lng: model[i].coordinates.lng
-//                     },
-//                     title: model[i].name
-//                 });
-//                 daz[i].infowindow = new google.maps.InfoWindow({content: model[i].description});
-//                 daz[i].setMap(map);
-//                 daz[i].addListener('click', function() {
-//                     this.infowindow.open(map, daz[i]);
-//                 })
-//             }
-//         };
-//
-//         populate();
-
-
-
-
 ////REACT/////
 
-var data = octopus.getMarkerNames();
+var EncompassingComponent = React.createClass({
 
+  getInitialState: function() {
+    return {
+      names: [
+        "Home", "Memphis BBQ"
+      ],
+      mapSettings: {
+      center: {
+        lat: 43.227,
+        lng: -79.719
+      },
+      zoom:11
+    }
+  }
+  },
 
-
-
-var List = React.createClass({
-    render: function() {
-        //loop through model, sandwich each name into <li>'s
-        var names = this.props.data.map(function(marker) {
-            return (
-                <a href="#">
-                    <h1 onClick={octopus.getMarkers}>{marker}</h1>
-                </a>
-            )
+  displayMarkers: function() {
+    let map = new google.maps.Map(document.getElementById('map'), this.state.mapSettings);
+      for (let i = 0; i < model.length; i++) {
+          model[i].marker = new google.maps.Marker({
+              position: {
+                  lat: model[i].coordinates.lat,
+                  lng: model[i].coordinates.lng
+              },
+              title: model[i].name
+          });
+        model[i].marker.infowindow = new google.maps.InfoWindow({content: model[i].description});
+        model[i].marker.setMap(map);
+        model[i].marker.addListener('click', function() {
+            this.infowindow.open(map, model[i].marker);
         })
-        return (
-            <div>
-                {names}
-            </div>
-        )
-    }
-});
+      }
 
-ReactDOM.render(
-    <div>
-    <List data={data}/>
-</div>, document.getElementById('markerList'));
 
-var Title = React.createClass({
-    render: function() {
-        return (
-            <div className="col-md-12">
-                <h1>{this.props.mainHeading}</h1>
-                <Description description='"make way for a scene"'/>
-            </div>
-        )
-    }
-});
+  },
 
-var Description = React.createClass({
-    render: function() {
-        return (
-            <p>{this.props.description}</p>
-        )
-    }
-});
-
-ReactDOM.render(
-    <div>
-    <Title mainHeading="Dog Parks in the Golden Horshoe"/>
-</div>, document.getElementById('title'));
-
-var Test = React.createClass({
-
-  render: function() {
-    var map = new google.maps.Map(document.getElementById('map'), middleMapSettings);
-// dynamically set the markers and the info window for them
-    function populate() {
-        var daz = [];
-        for (let i = 0; i < model.length; i++) {
-            daz[i] = new google.maps.Marker({
-                position: {
-                    lat: model[i].coordinates.lat,
-                    lng: model[i].coordinates.lng
-                },
-                title: model[i].name
-            });
-            daz[i].infowindow = new google.maps.InfoWindow({content: model[i].description});
-            daz[i].setMap(map);
-            daz[i].addListener('click', function() {
-                this.infowindow.open(map, daz[i]);
-            })
+  openMarker: function(i) {
+      this.setState({
+        mapSettings: {
+          center: {
+            lat: model[i].coordinates.lat,
+            lng: model[i].coordinates.lng
+          },
+          zoom: 16
         }
-    };
+      });
+  },
 
-    populate();
+  eachMarker: function(name, i) {
+    return (
+      <Marker key={i} index={i} openSesame={this.openMarker}>{name}</Marker>
+    )
+  },
 
+  //main component will have the other componentes inside of it
+  render: function() {
+    this.displayMarkers();
     return (
       <div>
-        {map}
+        <PageTitle />
+        <div className="markers">
+          {
+            this.state.names.map(this.eachMarker)
+          }
+        </div>
       </div>
     )
   }
 });
 
+var PageTitle = React.createClass({
+  render: function() {
+    return (
+      <div className="title">
+        <h1>Dog Parks in the Golden Horshoe</h1>
+        <h3>"Make way for a scene"</h3>
+      </div>
+
+    )
+  }
+});
+
+
+
+var Marker = React.createClass({
+
+  whatToDo: function() {
+    this.props.openSesame(this.props.index)
+  },
+
+  render: function() {
+    return(
+      <h3 onClick={this.whatToDo}>{this.props.children}</h3>
+    )
+  }
+});
+
 ReactDOM.render(
-  <div>
-  <Test />
-  </div>, document.getElementById('map')
+  <EncompassingComponent />, document.getElementById('main')
 );
